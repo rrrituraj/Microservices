78416ecd8753c52632df97178f9f06edf98f691b
Rituraj Gupta <riturajkbd@gmail.com>
2018-09-10 22:19:10 +0530

    added new project
    Signed-off-by: Rituraj Gupta <riturajkbd@gmail.com>

--- microservices/currency-conversion-service/src/main/java/com/microservices/demo/currencyConversionService/CurrencyExchangeServiceProxy.java
+++ microservices/currency-conversion-service/src/main/java/com/microservices/demo/currencyConversionService/CurrencyExchangeServiceProxy.java
@@ -7,11 +7,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 //@FeignClient(name="currency-exchange-service", url = "localhost:8000")
-@FeignClient(name="currency-exchange-service")
+//@FeignClient(name="currency-exchange-service")
+@FeignClient(name="netflix-zuul-api-gateway-server")
 @RibbonClient(name="currency-exchange-service")
 public interface CurrencyExchangeServiceProxy {
 
-	@RequestMapping(value = "/currency-exchange/from/{from}/to/{to}", method = RequestMethod.GET, produces = "application/json")
+	//@RequestMapping(value = "currency-exchange/from/{from}/to/{to}", method = RequestMethod.GET, produces = "application/json")
+	@RequestMapping(value = "currency-exchange-service/currency-exchange/from/{from}/to/{to}", method = RequestMethod.GET, produces = "application/json")
 	public CurrencyConversionBean retriveExchangeValue(@PathVariable("from") String from,
 			@PathVariable("to") String to); 
 	
