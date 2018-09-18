9d45e2cbe2b254e97593aff7e6984b46d8d23feb
Rituraj Gupta <riturajkbd@gmail.com>
2018-09-18 22:12:14 +0530

    commiting new project

---
+++ DefaultInheritanceStrategyExample/src/main/java/com/hbm/ritu/client/SaveDataClientTest.java
@@ -0,0 +1,48 @@
+package com.hbm.ritu.client;
+
+import java.math.BigDecimal;
+import java.text.ParseException;
+
+import com.hbm.ritu.entities.Employee;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+
+import com.hbm.ritu.entities.Person;
+import com.hbm.ritu.entities.Student;
+import com.hbm.ritu.util.HibernateUtil;
+
+public class SaveDataClientTest {
+
+	public static void main(String[] args) throws ParseException {
+	    try(Session session = HibernateUtil.getSessionFactory().openSession()) {
+	    	Person person = new Person();
+			person.setName("choosa");
+			person.setGender("Female");
+			
+			Employee employee = new Employee();
+			employee.setBonus(new BigDecimal("277.389"));
+			employee.setDeptName("IT");
+			employee.setDoj(HibernateUtil.getDoj("18/12/2015"));
+			employee.setEmail("dipesh.cs@gmail.com");
+			employee.setName("Dipesh");
+			employee.setSalary(80000.2872);
+			employee.setGender("Male");
+			
+			Student student = new Student();
+			student.setName("Shuruti");
+			student.setGender("Female");
+			student.setFee(20000.00f);
+			student.setSchoolName("DPS");
+			student.setSectionName("12th Std");
+			
+			session.beginTransaction();
+			session.save(person);
+			session.save(student);
+			session.save(employee);
+			
+			session.getTransaction().commit();
+		} catch (HibernateException e) {
+			e.printStackTrace();
+		}
+	  }
+}
